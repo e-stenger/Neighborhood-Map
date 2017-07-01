c5cddbe24dc49fc784dc747b83787fc8704d53f4
Eric <ericcstenger@gmail.com>
2017-06-30 19:14:44 -0500

    changed from foursquare api to wikipedia, but search is only going after the page of the first word in site.title, even though there is a page for the whole title

--- js/app.js
+++ js/app.js
@@ -1,97 +1,94 @@
 // open and close sideNav when explore button is clicked from hamburger menu notes Udacity
 var menu = document.querySelector('#menu');
-var main = document.querySelector('main');
+var main = document.querySelector('aside');
 var drawer = document.querySelector('.nav');
 main.addEventListener('click', function() {
     drawer.classList.toggle('open');
 });
 // locations array, list of sites
 var sites = [{
-        title: 'Giza Pyramids Ticket Office and Entrance',
+        title: "Giza Pyramid Complex",
         lat: "29.981907",
         lng: "31.132551",
-        url: "https://en.wikipedia.org/wiki/Giza_pyramid_complex",
-        //venueID: "4b9f7a50f964a520422537",
-        photoID: "590adae0dd12f83664f"
+        photo:""
     },
     {
-        title: 'The Great Pyramid at Giza',
+        title: "The Great Pyramid at Giza",
         lat: "29.979245",
         lng: "31.1342000",
-        url: "https://en.wikipedia.org/wiki/Great_Pyramid_of_Giza"
+        photo:""
     },
     {
-        title: 'Pyramid of Khafre',
+        title: "Pyramid of Khafre",
         lat: "29.976000",
         lng: "31.130784",
-        url: "https://en.wikipedia.org/wiki/Pyramid_of_Khafre"
+        photo:""
     },
     {
-        title: 'Great Sphinx of Giza',
+        title: "Great Sphinx of Giza",
         lat: "29.975300",
         lng: "31.137604",
-        url: "https://en.wikipedia.org/wiki/Great_Sphinx_of_Giza"
+        photo:""
     },
     {
-        title: 'Pyramid of Hetepheres I',
+        title: "Hetepheres I",
         lat: "29.978900",
         lng: "31.136229",
-        url: "https://en.wikipedia.org/wiki/Hetepheres_I"
+        photo:""
     },
     {
-        title: 'Khufu Ship',
+        title: "Khufu Ship",
         lat: "29.978000",
         lng: "31.134623",
-        url: "https://en.wikipedia.org/wiki/Khufu_ship"
+        photo:""
     },
     {
-        title: 'Giza Necropolis',
+        title: "Giza Necropolis",
         lat: "29.977311",
         lng: "31.132520",
-        url: "https://simple.wikipedia.org/wiki/Giza_Necropolis"
+        photo:""
     },
     {
-        title: 'Tomb of Hemiunu',
+        title: "Hemiunu",
         lat: "29.979366",
         lng: "31.129901",
-        url: "https://en.wikipedia.org/wiki/Hemiunu"
+        photo:""
     },
     {
-        title: 'Mortuary Temple of Khufu',
+        title: "Khufus Pyramid",
         lat: "29.979260",
         lng: "31.135675",
-        url: "https://en.wikipedia.org/wiki/Mortuary_temple"
+        photo:""
     },
     {
-        title: 'Rock Cut Tombs',
+        title: "Rock Cut Tombs",
         lat: "29.977248",
         lng: "31.129331",
-        url: "https://en.wikipedia.org/wiki/Rock-cut_tomb"
+        photo:""
     },
     {
-        title: 'Valley Temple of Khafre',
+        title: "Valley Temple of Khafre",
         lat: "29.974790",
         lng: "31.138358",
-        url: "https://sv.m.wikipedia.org/wiki/Valley_Temple_of_Khafre"
+        photo:""
     },
     {
-        title: 'Pyramid of Menkaure',
+        title: "Pyramid of Menkaure",
         lat: "29.972675",
         lng: "31.128523",
-        url: "https://en.wikipedia.org/wiki/Pyramid_of_Menkaure"
+        photo:""
     },
     {
-        title: 'Pyramids of Queens',
+        title: "Pyramid of Userkaf",
         lat: "29.971573",
         lng: "31.127991",
-        url: "https://sv.wikipedia.org/wiki/Pyramids_of_Queens"
+        photo: ""
     }
 ];
 var Location = function(data) {
     this.title = data.title;
     this.lat = data.lat;
     this.lng = data.lng;
-    this.url = data.url;
     this.marker = data.marker;
 };
 //init map
@@ -131,31 +128,30 @@ var ViewModel = function() {
     //toggleBounce, handleThis, markerClick function credit documentation and forum (modified)
     var toggleBounce = function(marker) { 
         marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function() {marker.setAnimation(null); }, 1500);
+        setTimeout(function() {marker.setAnimation(null); }, 1600);
     };
 
 var lastInfoWindow = null;
-self.handleThis = function(site, infoWindow) {
+self.handleThis = function(marker, infoWindow) {
     return function() {
         if (lastInfoWindow === infoWindow) {
-            toggleBounce(site.marker);
+            toggleBounce(marker);
             infoWindow.close(map, this);
             lastInfoWindow = null;
         } else {
             if (lastInfoWindow !== null) {
                 lastInfoWindow.close(map, this);
-                toggleBounce(site.marker);
+                toggleBounce(marker);
             }
-            toggleBounce(site.marker);
+            toggleBounce(marker);
             infoWindow.open(map, this);
             lastInfoWindow = infoWindow;
         }
-    infoWindow.setContent('<div class="infoWindow"><h2>' + site.title + '</h2>' + '<a href="' + site.url + '" target="_blank">' + site.url + '</a><br>' + site.siteImage + '<div>');
    };
 };
 
 // markerClick ties list item to cooresponding marker
-self.markerClick = function(location) {
+this.markerClick = function(location) {
     google.maps.event.trigger(location.marker, 'click');
     };
 
@@ -169,42 +165,35 @@ self.allSites.forEach(function(site) {
             var createMarkers = function() {
                 site.marker = new google.maps.Marker(markerOptions);
                 site.infoWindow = new google.maps.InfoWindow ({
-                    //content: '<h2>' + site.title + '</h2>' + '<a href="' + site.url + '" target="_blank">' + site.url + '</a>'
+                    content: '<h2>' + site.title + '</h2>' + '<div>'+ wikiElem +'</div>', maxWidth:300
                 }); 
                 
                 //listener opens infowindow and animates marker
-                google.maps.event.addListener(site.marker, 'click', self.handleThis(site, site.infoWindow));
+                google.maps.event.addListener(site.marker, 'click', self.handleThis(site.marker, site.infoWindow));
             };
 
-            createMarkers();
+            //createMarkers();
 
-// foursquare api info - trying to get working from forum post modified
-//var foursquareURL = "https://api.foursquare.com/v2/venues/' + site.venueID + '&/photos?' + site.photoID + '&client_id=M5AMWSRULFRYVIAV11A40HE1DYOLQBULFQHFR1X1HJUH1UII&client_secret=WHYWWD30H2V0Z4SN5PEBLMY4MN0QL0SO3ETSWIMFBP225WI0&v=20170620&m=foursquare";
-var foursquareURL = "https://foursquare.com/v/great-pyramids-of-giza/4b9f7a50f964a520422537e3?openPhotoId=' + site.photoID + '&client_id=M5AMWSRULFRYVIAV11A40HE1DYOLQBULFQHFR1X1HJUH1UII&client_secret=WHYWWD30H2V0Z4SN5PEBLMY4MN0QL0SO3ETSWIMFBP225WI0&v=20170620&m=foursquare";
-    var photo = [];
+//wikipedia api call
+var wikiElem = [];
 
-    function foursquarePhotos() {
-      $.ajax({
-        async: true,
-        url: foursquareURL,
-        dataType: "jsonp",
-        success: function(response) {
-          console.log(response);
-          var photos = response.response.photos;
-          var photo = photos.items[1];
-          var img = photo.prefix + 'width' + photo.width + photo.suffix;
-          //console.log(photos);
-          //console.log(photo);
-          var siteImage = ('<img class="siteImage" src="' + img + '">');
-          site.siteImage = siteImage;
-        },
-        error: function(e) {
-          console.log(e);
+var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + site.title + '&format=json&callback=wikiCallback';
+$.ajax(wikiUrl, {
+    dataType: 'jsonp' 
+}).done(function(response) {
+        console.log(response);
+        var articleList = response[1];
+        for (var i = 0; i < articleList.length; i++) {
+            articleStr = articleList [i];
+            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+            wikiElem.push('<li id="wikiLinks"><a href =' + url + '>' + articleStr + '</a><li>');
         }
-      });
-    }
-    foursquarePhotos();
+        createMarkers();
+    }).fail(function() {
+        wikiElem.push('<h3>Wikipedia failed to load info for location</h3>');
+    });
 
+});
 
 //search filter
 self.userInput = ko.observable('');
@@ -232,6 +221,6 @@ this.currentLocation = ko.observable();
 this.setLocation = function(site) {
     self.currentLocation(site);
 };
-});
 };
+
 
